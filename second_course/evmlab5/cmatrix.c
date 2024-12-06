5ec8036d645a1b98e2bb4c1b55338f38cc3a51e0
Alex Kirsanov <48834050+touitsu@users.noreply.github.com>
2024-12-06 19:45:34 +0700

    evmlab5

---
+++ second_course/evmlab5/cmatrix.c
@@ -0,0 +1,29 @@
+﻿#include <stdio.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <time.h>
+#include "cmatrix.h"
+
+
+int32_t main(int32_t argc, char** argv) {
+	float** matrix;
+	float** reverseMatrix;
+	float** multRes;
+	int32_t n, m;
+	clock_t start;
+
+	n = atoi(argv[1]);
+	m = atoi(argv[2]);
+
+	matrix = getRandomMatrix(n);
+
+	start = clock();
+	reverseMatrix = getReverseMatrix(matrix, n, m);
+
+	printf("Runtime: %lf seconds.\n", ((double)(clock() - start))/CLOCKS_PER_SEC);
+
+	freeMatrix(matrix, n);
+	freeMatrix(reverseMatrix, n);
+
+	return 0;
+}
